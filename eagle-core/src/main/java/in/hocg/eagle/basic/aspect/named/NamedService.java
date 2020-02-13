361cb96d08ba5842d333d2035717b1e244fa6972
hocgin <hocgin@gmail.com>
2020-02-13 19:41:10 +0800

    # 基础服务

--- eagle-core/src/main/java/in/hocg/eagle/basic/aspect/named/NamedService.java
+++ eagle-core/src/main/java/in/hocg/eagle/basic/aspect/named/NamedService.java
@@ -1,6 +1,11 @@
 package in.hocg.eagle.basic.aspect.named;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 /**
  * Created by hocgin on 2020/2/13.
@@ -9,9 +14,22 @@ import org.springframework.stereotype.Component;
  * @author hocgin
  */
 @Component
+@RequiredArgsConstructor(onConstructor = @__(@Lazy))
 public class NamedService {
     
+    /**
+     * 查询枚举值
+     *
+     * @param type
+     * @param id
+     * @return
+     */
     public Object selectOneByTypeAndId(String type, Integer id) {
+        if (Strings.isBlank(type) || Objects.isNull(id)) {
+            return null;
+        }
+        // TODO: 等待枚举
+        
         return null;
     }
 }
