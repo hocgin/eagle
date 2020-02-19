d4721dfd9bd52d9adfa6b80657befd3c84931501
hocgin <hocgin@gmail.com>
2020-02-19 23:14:09 +0800

    开发#SQL文件

--- eagle-core/src/main/resources/db/migration/V20200211.001__V0.1_table__account.sql
+++ eagle-core/src/main/resources/db/migration/V20200211.001__V0.1_table__account.sql
@@ -28,11 +28,11 @@ CREATE TABLE `t_account`
     --
     `created_at`      DATETIME(6)         NOT NULL
         COMMENT '创建时间',
-    `creator`         BIGINT UNSIGNED     NOT NULL
+    `creator`         BIGINT              NOT NULL
         COMMENT '创建者',
     `last_updated_at` DATETIME(6)
         COMMENT '更新时间',
-    `last_updater`    BIGINT UNSIGNED
+    `last_updater`    BIGINT
         COMMENT '更新者',
 
     PRIMARY KEY (`id`)
