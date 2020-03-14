f26b4d88d87db35dfa46fd082fb83b812d8cbf13
hocgin <hocgin@gmail.com>
2020-03-14 19:57:43 +0800

    TASK#shop模块+树形结构

--- eagle-core/src/main/java/in/hocg/eagle/modules/account/mapper/AuthorityMapper.java
+++ eagle-core/src/main/java/in/hocg/eagle/modules/account/mapper/AuthorityMapper.java
@@ -20,24 +20,6 @@ import java.util.List;
 public interface AuthorityMapper extends BaseMapper<Authority> {
 
     /**
-     * 根据 regex 匹配 tree_path 来更新节点的状态
-     *
-     * @param regexTreePath
-     */
-    int updateOffStatusByRegexTreePath(@Param("regexTreePath") String regexTreePath);
-
-    /**
-     * 指定新的 tree_path 并根据 regex 替换旧的 tree_path
-     *
-     * @param regexTreePath
-     * @param oldTreePath
-     * @param newTreePath
-     */
-    int updateTreePathByRegexTreePath(@Param("regexTreePath") String regexTreePath,
-                                      @Param("oldTreePath") String oldTreePath,
-                                      @Param("newTreePath") String newTreePath);
-
-    /**
      * 查找 parent_id 符合的数据
      *
      * @param qo
