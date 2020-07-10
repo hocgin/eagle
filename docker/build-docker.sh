448e87cc81a88aae48782c92e9bb80ec3da20add
hocgin <hocgin@gmail.com>
2020-07-10 20:34:00 +0800

    feature: 更新

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -2,4 +2,4 @@
 path=$(cd `dirname $0`; pwd)
 project=$(cd $path/..; pwd)
 mvn clean package -Dmaven.test.skip=true -f $project
-docker build --build-arg JAR_FILE=./eagle-core/target/*.jar -t eagle:latest -f $path/Dockerfile $project
+docker build --build-arg JAR_FILE=./eagle-core/target/*.jar -t eagle:latest -f $path/Dockerfile .
